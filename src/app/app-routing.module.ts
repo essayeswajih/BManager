6b09d22fa3f233a46ca08f4c5c457f55138b6959
essayeswajih <wajihsayes@gmail.com>
2024-07-01 11:03:40 +0100

    V0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SteComponent } from './pages/ste/ste.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:HomeComponent },
+  { path:'ste/:id',component:SteComponent},
+  { path:'ste/:id/page/id',component:SteComponent},
+  { path:'**',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SteComponent } from './pages/ste/ste.component';
+import { MesDoneeComponent } from './pages/mes-donee/mes-donee.component';
+import { ClientComponent } from './forms/client/client.component';
+import { ArticleComponent } from './forms/article/article.component';
+import { FamilleComponent } from './forms/famille/famille.component';
+import { DepotComponent } from './forms/depot/depot.component';
+import { FournisseurComponent } from './forms/fournisseur/fournisseur.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SteComponent,
+    MesDoneeComponent,
+    ClientComponent,
+    ArticleComponent,
+    FamilleComponent,
+    DepotComponent,
+    FournisseurComponent,
+    NavBarComponent,
+    TopBarComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    NgbModule,
+    FontAwesomeModule,
   ],
   providers: [
     provideClientHydration()
