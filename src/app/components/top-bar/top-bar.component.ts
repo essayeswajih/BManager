642494c689bc4fbf556acaf15ef26b69e050e61c
essayeswajih <wajihsayes@gmail.com>
2024-07-25 15:15:14 +0100

    prod_v001

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
+  constructor(private router :Router){}
+  open(url: string) {
+    this.router.navigateByUrl(url)
+  }
 
 }
