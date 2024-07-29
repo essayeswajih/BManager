49e0248b0fe5c8954f12b114a3e02743fa29962c
essayeswajih <wajihsayes@gmail.com>
2024-07-29 11:12:47 +0100

    prod_v001234

---
+++ src/app/pages/stock/stock.component.ts
@@ -0,0 +1,70 @@
+import { ToastrService } from 'ngx-toastr';
+import { SteService } from './../../apiServices/ste/ste.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock',
+  templateUrl: './stock.component.html',
+  styleUrls: ['./stock.component.scss']
+})
+export class StockComponent implements OnInit {
+  ArticlesList: any[] = [];
+  filteredArticles: any[] = [];
+  searchQuery: string = '';
+  sortBy: string = '0';
+
+  constructor(private ste: SteService, private toastr: ToastrService) {}
+
+  ngOnInit(): void {
+    this.ste.getStock().then(
+      (data: any) => {
+        this.ArticlesList = data;
+        this.filteredArticles = this.ArticlesList;
+        console.log(this.ArticlesList);
+      }
+    ).catch((error) => {
+      console.error('Error fetching stock:', error);
+      this.toastr.error('Failed to load stock data.');
+    });
+  }
+
+  onSearchChange(event: Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const searchValue = inputElement.value;
+    this.searchQuery = searchValue;
+    this.applyFilters();
+  }
+
+  onSortChange(event:Event): void {
+    const inputElement = event.target as HTMLInputElement;
+    const sortValue = inputElement.value;
+    this.sortBy = sortValue;
+    console.log(this.sortBy)
+    this.applyFilters();
+  }
+
+  private applyFilters(): void {
+    console.log('Search Query:', this.searchQuery);
+    let filtered = this.ArticlesList.filter(article => 
+      (article.article.designation || '').toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+
+    console.log('Filtered Articles:', filtered);
+    switch (this.sortBy) {
+      case '1': 
+        filtered.sort((a, b) => (a.article.achatHT ?? 0) - (b.article.achatHT ?? 0));
+        break;
+      case '2':
+        filtered.sort((a, b) => (a.qte ?? 0) - (b.qte ?? 0));
+        break;
+      default:
+        filtered.sort((a, b) => (a.article.designation || '').localeCompare(b.article.designation || ''));
+        break;
+    }
+  
+    this.filteredArticles = filtered;
+    console.log('Sorted Articles:', this.filteredArticles);
+  }
+  
+  
+}
