bb9c6d38ca49a64646ad6138975ffb5693273762
essayeswajih <wajihsayes@gmail.com>
2024-08-02 11:10:49 +0100

    prod_v002

--- src/app/components/new-bon-liv/new-bon-liv.component.ts
+++ src/app/components/new-bon-liv/new-bon-liv.component.ts
@@ -73,7 +73,7 @@ throw new Error('Method not implemented.');
       for(let i of this.items){
         if(i==item){
           i[key]=value;
-          i.totalNet = (i.article?.achatHT - (i.article?.achatHT * i.rem / 100)) * i.qte;
+          i.totalNet = (i.article?.achatHT - (i.article?.achatHT * i.remise / 100)) * i.qte;
         }
       }
     }else{
@@ -97,9 +97,9 @@ throw new Error('Method not implemented.');
     item.unite = article?.unite;
     item.puht = article?.achatHT;
     item.qte = qte || 1;
-    item.rem = rem || 0;
+    item.remise = rem || 0;
     item.tva = article?.tva;
-    item.totalNet = (article?.achatHT - (article?.achatHT * rem / 100)) * qte;
+    item.totalNet = (article?.achatHT - (article?.achatHT * item.remise / 100)) * qte;
     return item;
   }
   save() {
