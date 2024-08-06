c827170adf13323f53a912d1d42ecdb926523d99
essayeswajih <wajihsayes@gmail.com>
2024-08-06 11:27:31 +0100

    prod_v0021

--- src/app/pages/stocks/bla/bla.component.ts
+++ src/app/pages/stocks/bla/bla.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SteService } from '../../../apiServices/ste/ste.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-bla',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './bla.component.scss'
 })
 export class BlaComponent {
-
+  bonForm !:FormGroup;
+  bonList:any[] = [];
+  constructor(private fb :FormBuilder ,private tstr:ToastrService,private ste:SteService){
+    this.bonForm = fb.group({
+      'ClientName': [''],
+      'trans':['All']
+    })
+  }
+  ngOnInit(){
+    this.ste.getBonLivA().then(
+      (data:any)=>{
+        if (data.​status==200){
+          console.log(data)
+          this.bonList=data.data;
+        }
+      }
+    ).catch(
+      (error:any)=>{
+        this.tstr.error("INTERNAL SERVER ERROR","ERROR")
+      }
+    )
+  }
 }
