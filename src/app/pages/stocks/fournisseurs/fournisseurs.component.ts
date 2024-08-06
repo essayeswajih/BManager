a84efc15ff0a862fa6ff047e07de62e294b708d8
essayeswajih <wajihsayes@gmail.com>
2024-08-06 15:03:14 +0100

    prod_v0021

--- src/app/pages/stocks/fournisseurs/fournisseurs.component.ts
+++ src/app/pages/stocks/fournisseurs/fournisseurs.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { SteService } from '../../../apiServices/ste/ste.service';
 
 @Component({
   selector: 'app-fournisseurs',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './fournisseurs.component.scss'
 })
 export class FournisseursComponent {
-
+  clientForm !:FormGroup;
+  clientList:any[] = [];
+  constructor(private fb :FormBuilder ,private tstr:ToastrService,private ste:SteService){
+    this.clientForm = fb.group({
+      'ClientName': [''],
+      'trans':['All']
+    })
+  }
+  ngOnInit(){
+    this.ste.getFournisseurs().then(
+      (data:any[])=>{
+        if (data.length>0){
+          console.log(data)
+          this.clientList=data;
+        }
+      }
+    ).catch(
+      (error:any)=>{
+        this.tstr.error("INTERNAL SERVER ERROR","ERROR")
+      }
+    )
+  }
 }
