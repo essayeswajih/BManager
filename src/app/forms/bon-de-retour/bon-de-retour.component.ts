5332793734ef87e7489f56d43b85c2b09c4f5aa4
essayeswajih <wajihsayes@gmail.com>
2024-08-08 10:28:10 +0100

    prod_v0022

--- src/app/forms/bon-de-retour/bon-de-retour.component.ts
+++ src/app/forms/bon-de-retour/bon-de-retour.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SteService } from '../../apiServices/ste/ste.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-bon-de-retour',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './bon-de-retour.component.scss'
 })
 export class BonDeRetourComponent {
-
+  search!:FormGroup;
+  bon:any = {};
+  constructor(private fb:FormBuilder,private ste:SteService,private tstr:ToastrService){
+    this.search = this.fb.group({
+      'id': ['',Validators.required],
+    })
+  }
 }
