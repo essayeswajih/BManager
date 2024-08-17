2f0aed2c4d9ba468f259cdfe566a86b0d239edf9
essayeswajih <wajihsayes@gmail.com>
2024-08-17 12:49:37 +0100

    x

--- src/app/components/bon-liv-a-details/bon-liv-a-details.component.ts
+++ src/app/components/bon-liv-a-details/bon-liv-a-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SteService } from '../../apiServices/ste/ste.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-bon-liv-a-details',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './bon-liv-a-details.component.scss'
 })
 export class BonLivADetailsComponent {
+  @Input() bonLiv: any = {}; 
+  historiqueBonLiv: any = {};
 
+  constructor(private ste: SteService, private toastr: ToastrService) {}
+
+  ngOnChanges() {
+    this.fetchArticle(); 
+  }
+
+  fetchArticle() {
+    if (this.bonLiv && this.bonLiv.id) {
+      const idBonLiv = this.bonLiv.id;
+
+      /*this.ste.getHistoriqueBonLivA(idArticle).then(
+        (response) => {
+          this.historiqueBonLiv = response;
+          console.log("Historique article : ", this.historiqueBonLiv);
+        }
+      ).catch(
+        (error) => {
+          console.log(error);
+          this.toastr.error('Failed to fetch article history');
+        }
+      );
+      */
+    } 
+  }
 }
